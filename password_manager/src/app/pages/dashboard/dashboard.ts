28ee417e2eae1daf1859a1549486706056663ae4
Enzo PENISSON <104147716+EnzoPENISSON@users.noreply.github.com>
2024-06-24 19:42:26 +0000

    design vault

--- password_manager/src/app/pages/dashboard/dashboard.ts
+++ password_manager/src/app/pages/dashboard/dashboard.ts
@@ -29,9 +29,13 @@ import { newVault } from '../../models/newVault.models';
 import { DropdownModule } from 'primeng/dropdown';
 import { categorie, categories } from '../../models/categories.models';
 import { addCategorie } from '../../models/addCategorie.models';
+import { AvatarModule } from 'primeng/avatar';
+import { AvatarGroupModule } from 'primeng/avatargroup';
 @Component({
   standalone: true,
   imports: [
+    AvatarModule,
+    AvatarGroupModule,
     RouterOutlet,
     InputTextModule,
     PasswordModule,
@@ -47,118 +51,133 @@ import { addCategorie } from '../../models/addCategorie.models';
     DialogModule,
     DropdownModule
   ],
-  providers:[MessageService, BrowserModule],
+  providers: [MessageService, BrowserModule],
   templateUrl: './dashboard.html',
   styleUrl: './dashboard.scss',
 })
 export class DashboardComponent {
-    loginData!: loginRegister;
-    infoCoffre!:infoCoffre;
-    newVault!:newVault;
-    listCategories!:categorie[];
-    listelibCategorie!:string[];
-    listeidCategorie!:string[];
-    sidebarVisible=false;
-    addMenuVault=false;
-    visibleAddCategorie=false;
-    newCategorie="";
+  loginData!: loginRegister;
+  infoCoffre!: infoCoffre;
+  newVault!: newVault;
+  listCategories!: categorie[];
+  listelibCategorie!: string[];
+  listeidCategorie!: string[];
+  sidebarVisible = false;
+  addMenuVault = false;
+  edit = false;
+  visibleAddCategorie = false;
+  newCategorie = "";
 
   constructor(private loginRegisterService: LoginRegisterService,
-    private dashboardService:DashboardService,
-    private router :Router,
+    private dashboardService: DashboardService,
+    private router: Router,
     private messageService: MessageService
   ) {
   }
-  vaultListe:Array<vaultListe>=[]
+  vaultListe: Array<vaultListe> = []
   ngOnInit(): void {
 
     this.loginData = this.loginRegisterService.getLoginData();  // Récupérer les données de connexion
     console.log(this.loginData)
     this.dashboardService.ShowAllVault(this.loginData.access_token).subscribe({
       next: (response: vaultListe[]) => {
-      
-          this.vaultListe=response;
+
+        this.vaultListe = response;
       }
     },
-  )}
+    )
+  }
 
-  disconnect(acces_token:string){
+  disconnect(acces_token: string) {
     this.dashboardService.disconnect(acces_token).subscribe({
-        next: (response: logout) => {
-        
-            console.log('Authentification successful', response);
-            this.router.navigate(['/']);
-    
-        },
-        error: (error) => {
-          console.error('Authentification error', error);
-        },
-      });
+      next: (response: logout) => {
+
+        console.log('Authentification successful', response);
+        this.router.navigate(['/']);
+
+      },
+      error: (error) => {
+        console.error('Authentification error', error);
+      },
+    });
 
 
-}
+  }
 
-InfoCoffre(uuidCoffre: string) {
-  this.dashboardService.SecretKey(uuidCoffre, this.loginData.access_token).pipe(
-    switchMap((secretkey: secretkey) => {
-      if (secretkey) {
-        return this.dashboardService.getInfoCoffre(uuidCoffre, secretkey.key, this.loginData.access_token).pipe(
-          tap((infoCoffre: infoCoffre) => {
-            this.infoCoffre = infoCoffre;
-            this.sidebarVisible = true;
-          })
-        );
-      } else {
-        // Gérer le cas où secretkey est null
-        return throwError('La clé secrète est null');
-      }
-    }),
-    catchError(error => {
-      console.error('Erreur:', error);
-      return throwError(error);
-    })
-  ).subscribe();
-}
+  InfoCoffre(uuidCoffre: string) {
+    this.dashboardService.SecretKey(uuidCoffre, this.loginData.access_token).pipe(
+      switchMap((secretkey: secretkey) => {
+        if (secretkey) {
+          return this.dashboardService.getInfoCoffre(uuidCoffre, secretkey.key, this.loginData.access_token).pipe(
+            tap((infoCoffre: infoCoffre) => {
+              this.infoCoffre = infoCoffre;
+              this.sidebarVisible = true;
+            })
+          );
+        } else {
+          // Gérer le cas où secretkey est null
+          return throwError('La clé secrète est null');
+        }
+      }),
+      catchError(error => {
+        console.error('Erreur:', error);
+        return throwError(error);
+      })
+    ).subscribe();
+  }
 
-  AddVault(){
-    this.addMenuVault=true;
+  AddVault() {
+    this.addMenuVault = true;
     this.dashboardService.getCategorie(this.loginData.access_token).subscribe({
       next: (response: categories) => {
-      
-        this.listCategories=response.categories;
+
+        this.listCategories = response.categories;
         this.listCategories.forEach(categorie => {
           this.listelibCategorie.push(categorie.libCategorie)
           this.listeidCategorie.push(categorie.idCategorie)
           console.log(this.listCategories)
         });
-          
+
       }
     },
-  )}
-
-  showAddCategorie(){
-    this.visibleAddCategorie= true;
+    )
   }
 
-  addNewCategorie(){
+  showAddCategorie() {
+    this.visibleAddCategorie = true;
+  }
+
+  addNewCategorie() {
     this.dashboardService.addNewCategorie(this.loginData.access_token, this.newCategorie).subscribe({
       next: (response: addCategorie) => {
         console.log(response.status)
-       
-          this.visibleAddCategorie= false;
-          this.messageService.add({ severity: 'success', summary: 'Nouvelle Catégorie', detail: 'Un nouvelle catégorie à été ajouté ' });
 
-       
+        this.visibleAddCategorie = false;
+        this.messageService.add({ severity: 'success', summary: 'Nouvelle Catégorie', detail: 'Un nouvelle catégorie à été ajouté ' });
 
-    },
-    error: (error) => {
-      console.error('Authentification error', error);
-    },
+
+
+      },
+      error: (error) => {
+        console.error('Authentification error', error);
+      },
     })
   }
 
-  onSidebarHide(){
-    this.sidebarVisible=false
+  onSidebarHide() {
+    this.sidebarVisible = false
+  }
+  onSave() {
+    // Implement save functionality here
+    console.log('Save', this.infoCoffre);
   }
 
+  onCancel() {
+    // Implement cancel functionality here
+    this.sidebarVisible = false;
+  }
+
+  editVault() {
+    this.edit = true;
+  }
 }
